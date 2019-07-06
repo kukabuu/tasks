1eed4e60c944de56a4d92d5d93cc124d8882a8da
Ekaterina Blinova <kukabuu@gmail.com>
2019-07-06 13:44:27 +0300

    Find sum of arguments like as sum(1)(2)(3)=6

---
+++ sumArguments.js
@@ -0,0 +1,16 @@
+function sum(a) {
+	var currentSum = a;
+
+	function f(b) {
+		currentSum += b;
+		return f;
+	}
+
+	f.toString = function() {
+		return currentSum;
+	};
+
+	return f;
+}
+
+console.log(sum(1)(2));
