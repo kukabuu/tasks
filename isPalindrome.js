3eb1d5d68d05d5bab7d5cbc19576b88324390fa6
Ekaterina Blinova <kukabuu@gmail.com>
2019-07-10 14:39:53 +0300

    Add new solution with sentences

--- isPalindrome.js
+++ isPalindrome.js
@@ -24,3 +24,23 @@ var isPalindrome2 = function(x) {
 	if (x == y || x == Math.floor(y / 10)) return true;
 	else return false;
 };
+
+//use senteces
+var isPalindrome = function(s) {
+	if (s.length === 0) return true;
+
+	let regExp = /[a-z0-9]/g;
+	let arr = s.toLowerCase().match(regExp);
+	if (arr === null) return true;
+
+	for (i = 0; i < arr.length / 2; i++) {
+		if (arr[i] !== arr[arr.length - 1 - i]) {
+			return false;
+		}
+	}
+	return true;
+};
+
+console.log(isPalindrome("A man, a plan, a canal: Panama")); //true
+console.log(isPalindrome("./")); //true;
+console.log(isPalindrome(""race a car"")); //false;
