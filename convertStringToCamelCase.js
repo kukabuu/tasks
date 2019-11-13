b53aafc2a6588af1641c273f898d4bc90d415df9
kukabuu <kukabuu@gmail.com>
2019-11-13 11:36:58 +0300

    Convert string to camel case using regexp

---
+++ convertStringToCamelCase.js
@@ -0,0 +1,14 @@
+/*
+Complete the method/function so that it converts dash/underscore delimited words into camel casing. The first word within the output should be capitalized only if the original word was capitalized (known as Upper Camel Case, also often referred to as Pascal case).
+
+Examples
+toCamelCase("the-stealth-warrior") // returns "theStealthWarrior"
+
+toCamelCase("The_Stealth_Warrior") // returns "TheStealthWarrior"
+*/
+
+function toCamelCase(str) {
+	return str.replace(/[^a-zA-Z0-9]+(.)/g, (match, p1) => p1.toUpperCase());
+}
+
+toCamelCase("the-stealth-warrior");
